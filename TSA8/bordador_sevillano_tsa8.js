026a3d8af0d21ee1a74c777d5ddb8bb305f8f0c6
SophiaSevillano <154325710+SophiaSevillano@users.noreply.github.com>
2024-07-01 10:38:14 +0800

    Create bordador_sevillano_tsa8.js

---
+++ TSA8/bordador_sevillano_tsa8.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const flexContainer = document.querySelector('.flex-container');
+    const gapInput = document.getElementById('gap-value');
+    const box1GrowInput = document.getElementById('grow-box1-value');
+    const box2GrowInput = document.getElementById('grow-box2-value');
+    const box3GrowInput = document.getElementById('grow-box3-value');
